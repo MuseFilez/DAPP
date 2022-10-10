801308b9cce55f2f0375c92454e7d4da65fa6575
Lamont V <lxveal1983@gmail.com>
2022-10-10 06:57:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         <p>Meet the Muse Filez team!</p>
       </h1>
         <div class="team-info">
-            <img src="img-1.jpg" alt="" width="500" height="500"/>
+            <img src="img-1.JPG" alt="" width="500" height="500"/>
             <h4><a href="https://app.unlock-protocol.com/checkout?redirectUri=https%3A%2F%2Fdry-firefly-0619.on.fleek.co%2F&paywallConfig=%7B%22locks%22%3A%7B%220x91AEa22A43138C5B3ebFf0721986DF02af816b11%22%3A%7B%22network%22%3A4%7D%7D%2C%22messageToSign%22%3A%22Hello%20world%22%2C%22pessimistic%22%3Atrue%2C%22persistentCheckout%22%3Atrue%2C%22icon%22%3A%22https%3A%2F%2Flocksmith.unlock-protocol.com%2Flock%2F0x91AEa22A43138C5B3ebFf0721986DF02af816b11%2Ficon%22%7D" target="_blank">Pete Myers</a></h4>
              <p>Recording Artist/Producer</p>
         </div>
